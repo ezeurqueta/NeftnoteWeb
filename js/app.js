719dbc653d50351598b1aac448a5bca705bda944
ivanln26 <ivanluis.cba@gmail.com>
2017-10-11 00:16:05 -0300

    New navbar, and starting carousel

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+window.onload = function() {
+   var c =document.getElementById("canvas");
+   var ctx = c.getContext("2d");
+   var img = document.getElementById("bb");
+   ctx.drawImage(img, 0, 0);
+};
