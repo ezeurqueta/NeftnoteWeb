c372d19334013b89f21149ebb9dd0e11e67190bc
ivanln26 <ivanluis.cba@gmail.com>
2017-10-12 10:14:57 -0300

    changeURL() added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 // Script para que el carousel se mueva a determinado tiempo
 $('.carousel').carousel({
-    interval: 1000;
+    interval: 1000
+});
+function cargador(carga) {
+    $("#el_contenedor").load(carga);
+}
+$( document ).ready(function() {
+    cargador("home.html");
 });
