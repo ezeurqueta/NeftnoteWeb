01b17b03cddd8eab03f8afad734fdd57c93f1118
Ivan Nuñez <ivanluis.cba@gmail.com>
2017-11-06 23:57:02 -0300

    movie & profile .html added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var user;
+
 // funciones
 
 function cargador(carga) {
@@ -8,20 +10,33 @@ function cargador(carga) {
               }
           });
         deleteBackground(carga);
+    } else if (carga == "movies"){
+        $("#el_contenedor").load(carga + '.html' ,  function( response, status ) {
+              if ( status != "error" ) {
+                  loadMovies();
+              }
+          });
+        deleteBackground(carga);
+    } else if (carga == "profile") {
+        $("#el_contenedor").load(carga + '.html' ,  function( response, status ) {
+              if ( status != "error" ) {
+                  showProfile(user);
+              }
+          });
     } else if (carga != "search") {
         $("#el_contenedor").load(carga + '.html');
         deleteBackground(carga);
     } else {
         if (document.getElementById('searchbar').value != ""){
             $("#el_contenedor").load(carga + '.html');
-            // var what = document.getElementById('searchbar').value;
+            // let what = document.getElementById('searchbar').value;
         }
     }
 }
 
 function deleteBackground(carga) {
     let navbar_values = ["home", "series", "movies", "help"];
-    for (var i = 0; i < navbar_values.length; i++) {
+    for (let i = 0; i < navbar_values.length; i++) {
         if (navbar_values[i] == carga) { $("#" + carga).addClass("activate"); }
         else { $("#" + navbar_values[i]).removeClass("activate"); }
     }
@@ -47,15 +62,15 @@ function logMenu() {
 }
 
 function register() {
-    var usr_name = $('#txt_usr_name').val();
-    var f_name = $('#txt_f_name').val();
-    var l_name = $('#txt_l_name').val();
-    var e_mail = $('#txt_e_mail').val();
-    var phone = $('#txt_phone').val();
-    var re_pass = $('#txt_re_pass').val();
-    var re_cpass = $('#txt_re_cpass').val();
+    let usr_name = $('#txt_usr_name').val();
+    let f_name = $('#txt_f_name').val();
+    let l_name = $('#txt_l_name').val();
+    let e_mail = $('#txt_e_mail').val();
+    let phone = $('#txt_phone').val();
+    let re_pass = $('#txt_re_pass').val();
+    let re_cpass = $('#txt_re_cpass').val();
 
-    var db_register = firebase.database();
+    let db_register = firebase.database();
 
     db_register.ref('/users/').push({
         'f_name':f_name,
@@ -72,23 +87,22 @@ function register() {
 }
 
 function submit() {
-    var usr = document.getElementById('usr').value;
-    var pass = document.getElementById('pass').value;
+    let usr = document.getElementById('usr').value;
+    let pass = document.getElementById('pass').value;
     console.log(usr + " " + pass);
 }
 
 function logGoogle() {
-    var provider = new firebase.auth.GoogleAuthProvider();
+    let provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+        let token = result.credential.accessToken;
         // The signed-in user info.
-        var user = result.user;
-        console.log(user);
+        user = result.user;
 
-        var name = user.displayName;
-        var photo = user.photoURL;
-        var email = user.email;
+        let name = user.displayName;
+        let photo = user.photoURL;
+        let email = user.email;
 
         $('.ulist').html('').append("<li><a class='photo_href' href='#'><img id='photo'></img></a></li>" +
         "<li><a href='#'><button id='usr_name' class='btn btn-secondary dropdown-toggle' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'></button></a></li>" +
@@ -99,29 +113,38 @@ function logGoogle() {
         $('#usr_name').text(name);
         $('#photo').css('display', 'block');
         $('#photo').attr('src', photo);
-        $('.photo_href').attr('href', photo);
+        $('.photo_href').attr('onclick', "cargador('profile')");
         cargador("home");
 
     }).catch(function(error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // The email of the user's account used.
-        var email = error.email;
+        let email = error.email;
         // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+        let credential = error.credential;
         // ...
     });
 }
 
-function loadSeries() {
-    var series = $('#series_list');
+function showProfile(user) {
+    console.log(user);
+    $('#profile_img').attr('src', user.photoURL);
+    $('#profile_img').css('width', '100px');
+    $('#profile_img').css('height', '100px');
+    $('#profile_img').css('border-radius', '20px');
+    $('#profile_name').text('Profile name: ' + user.displayName)
+}
 
-    var dbSeries = firebase.database().ref('/series/');
+function loadSeries() {
+    let series = $('#series_list');
+
+    let dbSeries = firebase.database().ref('/series/');
     dbSeries.on('value', snapshot => {
         snapshot.forEach(snap => {
-            var div = document.createElement('div');
-            var input = document.createElement('input');
+            let div = document.createElement('div');
+            let input = document.createElement('input');
             div.classList = "col-lg-3 col-md-4 col-sm-6 portfolio-item";
             input.type = "image";
             input.src = snap.val().photoURL;
@@ -135,32 +158,75 @@ function loadSeries() {
     })
 }
 
-function loadInfoSerie(series) {
+function loadInfoSerie(serie) {
     $("#el_contenedor").load("serie.html", function( response, status ) {
-          if ( status != "error" ) {
-              const dbX = firebase.database().ref("/series/" + series);
-              dbX.on('value', snap => {
-                  let info = document.getElementById("info");
-                  let input = document.createElement("input");
-                //   let h2 = document.createElement("h2");
-                //   h2.innerText = "Title" + snap.val().name;
-                //   info.append(h2);
+        if ( status != "error" ) {
+            const dbX = firebase.database().ref("/series/" + serie);
+            dbX.on('value', snap => {
+                let info = document.getElementById("info");
+                let input = document.createElement("input");
+                    //   let h2 = document.createElement("h2");
+                    //   h2.innerText = "Title" + snap.val().name;
+                    //   info.append(h2);
 
-                  input.type = "image";
-                  input.src = snap.val().photoURL;
-                  input.style.width = "320px";
-                  input.style.height = "400px";
-                  info.append(input);
+                input.type = "image";
+                input.src = snap.val().photoURL;
+                input.style.width = "320px";
+                input.style.height = "400px";
+                info.append(input);
+                $("#name").text("Title: " + snap.val().name);
+                $("#director").text("Director: " + snap.val().director);
+            })
+        }
+    });
+}
 
-                  $("#name").text("Title: " + snap.val().name);
-                  $("#director").text("Director: " + snap.val().director);
-              })
-          }
-      });
+function loadMovies() {
+    let movies = $('#movies_list');
+
+    const dbMovies = firebase.database().ref('/movies/');
+    dbMovies.on('value', snapshot => {
+        snapshot.forEach(snap => {
+            let div = document.createElement('div');
+            let input = document.createElement('input');
+            div.classList = "col-lg-3 col-md-4 col-sm-6 portfolio-item";
+            input.type = "image";
+            input.src = snap.val().photoURL;
+            input.style.width = "320px";
+            input.style.height = "400px";
+            input.style.marginLeft = "10px";
+            input.onclick = e => {loadInfoMovie(snap.key)};
+            div.append(input);
+            movies.append(div);
+        })
+    })
+}
+
+function loadInfoMovie(movie) {
+    $("#el_contenedor").load("movie.html", function( response, status ) {
+        if ( status != "error" ) {
+            const dbX = firebase.database().ref("/movies/" + movie);
+            dbX.on('value', snap => {
+                let info = document.getElementById("info");
+                let input = document.createElement("input");
+                    //   let h2 = document.createElement("h2");
+                    //   h2.innerText = "Title" + snap.val().name;
+                    //   info.append(h2);
+
+                input.type = "image";
+                input.src = snap.val().photoURL;
+                input.style.width = "320px";
+                input.style.height = "400px";
+                info.append(input);
+                $("#name").text("Title: " + snap.val().name);
+                $("#director").text("Director: " + snap.val().director);
+            })
+        }
+    });
 }
 
 $( document ).ready(function() {
-    var config = {
+    let config = {
        apiKey: "AIzaSyCkN2-SkpibvX6T2bvB5ZtUWZLaQSqNqB0",
        authDomain: "neftnoteweb.firebaseapp.com",
        databaseURL: "https://neftnoteweb.firebaseio.com",
@@ -173,6 +239,6 @@ $( document ).ready(function() {
     cargador("home");
 
     $('.carousel').carousel({
-        interval: 1000
+        interval: 100
     });
 });
