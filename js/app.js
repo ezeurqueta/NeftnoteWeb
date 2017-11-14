462df2a2a337c18921ebc0be7fc63b24df88db5b
Ivan Nuñez <ivanluis.cba@gmail.com>
2017-11-13 23:02:43 -0300

    sm status added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-var user;
-
 // funciones
 
 function loader(carga) {
@@ -26,14 +24,9 @@ function loader(carga) {
     } else if (carga == "help"){
         $("#el_contenedor").load(carga + '.html');
         deleteBackground(carga);
-    } else if (carga != "search") {
+    } else {
         $("#el_contenedor").load(carga + '.html');
         deleteBackground(carga);
-    } else {
-        if (document.getElementById('searchbar').value != ""){
-            $("#el_contenedor").load(carga + '.html');
-            // let what = document.getElementById('searchbar').value;
-        }
     }
 }
 
@@ -72,6 +65,22 @@ function searchX(x) {
     });
 }
 
+function searchBar(x, val) {
+    let fbdb = firebase.database().ref(`/${x}/`);
+    fbdb.on('value', snapshot => {
+        snapshot.forEach(snap => {
+            if (snap.val().name.toLowerCase().includes(val.toLowerCase()) && val != '') {
+                console.log(snap.val().name);
+            }
+        });
+    });
+}
+
+$('#searchbar').keyup( e => {
+    let val = $('#searchbar').val();
+    searchBar('series', val);
+});
+
 function deleteBackground(carga) {
     let navbar_values = ["home", "series", "movies", "help"];
     for (let i = 0; i < navbar_values.length; i++) {
@@ -88,7 +97,7 @@ function hideOptions() {
 
 function showLogMenu() {
     $('.ulist').append('<li><a><input type="image" src="img/google.png" onclick="logGoogle();" width="30" height="30"></input></a></li>' +
-    "<li><a><button onclick='loader()'>Register</button></a></li>" +
+    `<li><a><button onclick="loader('signUp')">Register</button></a></li>` +
     '<li><a><button>Log In</button></a></li>' +
     '<li><a style="color:white">Password: <input type="password" style="width: 120px"></a></li>'+
     '<li><a style="color: white">Username: <input style="width: 120px"></a></li>')
@@ -143,28 +152,67 @@ function logGoogle() {
         let photo = user.photoURL;
 
         let emails = [];
+        let keys = [];
 
         firebase.database().ref('/users/').once('value', snap => {
             snap.forEach(s => {
                 emails.push(s.val().e_mail);
+                console.log(s.key);
+                keys.push(s.key);
             })
             if (emails.includes(email)) {
                 alert("Logged");
+                console.log(keys);
             } else {
                 firebase.database().ref('/users/').push({
                     'name': name,
-                    'e_mail': email
+                    'e_mail': email,
+                    'profile': {
+
+                    }
                 });
                 alert("Account created");
+console.log(keys);
             }
         });
 
+        // $('.ulist').html('').append("<li><a class='photo_href' href='#'><img id='photo'></img></a></li>" +
+        // "<li><a href='#'><button id='usr_name' class='btn btn-secondary dropdown-toggle' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'></button></a></li>" +
+        // "<div class='dropdown-menu' aria-labelledby='dropdownMenuButton'><a class='dropdown-item'>Action</a><a class='dropdown-item'>Action</a><a class='dropdown-item'>Action</a></div>" +
+        // "</a></li>");
 
+        const li_img = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+        const div = document.createElement('div');
+        const btn = document.createElement('button');
+        const div_items = document.createElement('div')
+        const a1 = document.createElement('a');
+        const a2 = document.createElement('a');
 
-        $('.ulist').html('').append("<li><a class='photo_href' href='#'><img id='photo'></img></a></li>" +
-        "<li><a href='#'><button id='usr_name' class='btn btn-secondary dropdown-toggle' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'></button></a></li>" +
-        "<div class='dropdown-menu' aria-labelledby='dropdownMenuButton'><a class='dropdown-item'>Action</a><a class='dropdown-item'>Action</a><a class='dropdown-item'>Action</a></div>" +
-        "</a></li>");
+        $(a).attr('class', 'photo_href');
+        $(img).attr('id', 'photo');
+        $(div).attr('class', 'dropdown');
+        $(btn).attr('onclick', 'showDDMenu()');
+        $(btn).attr('class', 'dropbtn');
+        $(btn).text(`${name} 🡻`);
+        $(a1).text("Profile");
+        $(a1).attr('href', "#");
+        $(a1).attr('onclick', "loader('profile')");
+        $(a2).text("Log Out");
+        $(a2).attr('href', "#");
+        $(a2).attr('onclick', "logOutGoogle()");
+        $(div_items).attr('id', 'myDropdown');
+        $(div_items).attr('class', 'dropdown-content');
+
+        $(a).append(img);
+        $(li_img).append(a);
+        $(div).append(btn);
+        $(div_items).append(a1);
+        $(div_items).append(a2);
+        $(div).append(div_items);
+        $('.ulist').html('').append(li_img);
+        $('.ulist').append(div);
 
         $('#usr_name').css('display', 'block');
         $('#usr_name').text(name);
@@ -185,6 +233,18 @@ function logGoogle() {
     });
 }
 
+function logOutGoogle() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        resetProfile();
+        user = undefined;
+        loader('index');
+        alert("Logged out");
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
+
 function showProfile(user) {
     console.log(user);
     $('#profile_img').attr('src', user.photoURL);
@@ -192,6 +252,46 @@ function showProfile(user) {
     $('#profile_img').css('height', '100px');
     $('#profile_img').css('border-radius', '20px');
     $('#profile_name').text('Profile name: ' + user.displayName)
+}
+
+function showDDMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function resetProfile() {
+    const liL = document.createElement('li');
+    const aL = document.createElement('a');
+    const liS = document.createElement('li');
+    const aS = document.createElement('a');
+
+    $(aL).attr('id', 'login');
+    $(aL).attr('onclick', 'logMenu()');
+    $(aL).attr('href', '#');
+    $(aL).text("Log In");
+    $(aS).attr('id', 'signUp');
+    $(aS).attr('onclick', "loader('signUp')");
+    $(aS).attr('href', '#');
+    $(aS).text("Sign Up");
+
+    $(liL).append(aL);
+    $(liS).append(aS);
+    $('.ulist').html('').append(liL);
+    $('.ulist').append(liS);
 }
 
 function loadList(x) {
@@ -225,9 +325,34 @@ function loadInfo(x, key) {
                 $("#name").text("Title: " + snap.val().name);
                 $("#director").text("Director: " + snap.val().director);
             })
-            $("#btn_back").attr("onclick", `loader('${x}')`)
+            $("#btn_back").attr("onclick", `loader('${x}')`);
+            smkey = key;
+            loadInfoUser(key);
         }
     });
+}
+
+function loadInfoUser(key) {
+    const db_user = firebase.database().ref('/users/-Kys7TX0xQy6DVKRFNCl/profile/' + key);
+    db_user.on('value', snap => {
+        $(`#sm_status option:contains(${snap.val().status})`).attr('selected', true);
+    });
+}
+
+function updateProfile() {
+    if (user != undefined) {
+        console.log("updated");
+        let sm_status = $("#sm_status option:selected").text();
+        console.log(smkey);
+        const db_user = firebase.database().ref('/users/-Kys7TX0xQy6DVKRFNCl/profile');
+        db_user.update({
+            [smkey]: {
+                'status': sm_status
+            }
+        });
+    } else {
+        console.log("nope");
+    }
 }
 
 $( document ).ready(function() {
@@ -240,6 +365,10 @@ $( document ).ready(function() {
        messagingSenderId: "117793164770"
      };
     firebase.initializeApp(config);
+
+    let user;
+    let userkey;
+    let smkey;
 
     loader("home");
 
